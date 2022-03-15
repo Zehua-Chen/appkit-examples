2d727ff48db18e81dccf03314e8da8899ecc2654
Zehua-Chen <zehua-chen@outlook.com>
2022-03-15 18:05:43 -0400

    [feat] Create Related Items

---
+++ RelatedItemExample/RelatedItemExample/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  RelatedItemExample
+//
+//  Created by Zehua Chen on 3/15/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+  func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+    return true
+  }
+
+
+}
+
---
+++ RelatedItemExample/RelatedItemExample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  RelatedItemExample
+//
+//  Created by Zehua Chen on 3/15/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
