553dd37a5f8bfc585e4c96966aabd45b64917759
Zehua-Chen <zehua-chen@outlook.com>
2022-03-26 14:43:15 -0400

    [feat] Ensure SavePanel is always shown regardless of many window there are

--- BookMaker/BookMaker/AppDelegate.swift
+++ BookMaker/BookMaker/AppDelegate.swift
@@ -36,6 +36,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     return false
   }
   
+  // FIXME: don't show welcome window when there is already a document
   func showWelcomeWindowIfNeeded() {
     let windowCount = NSApplication.shared.windows.count
     
@@ -47,31 +48,42 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
   }
   
+  @MainActor
+  func selectURLForNewDocument() async throws -> (NSApplication.ModalResponse, URL?) {
+    let panel = NSSavePanel()
+    panel.allowedContentTypes = [UTType("com.zehuachen-examples.book")!]
+    
+    if let window = NSApplication.shared.mainWindow {
+      let response = await panel.beginSheetModal(for: window)
+      
+      return (response, panel.url)
+    }
+    
+    let response = panel.runModal()
+    return (response, panel.url)
+  }
+  
   @IBAction func newDocument(_ sender: Any?) {
     Task {
-      let panel = NSSavePanel()
-      panel.allowedContentTypes = [UTType("com.zehuachen-examples.book")!]
-        
+      let (response, url) = try! await selectURLForNewDocument()
+      
       let fileManager = FileManager.default
       
-      // TODO: run modal when there is no window
-      // TODO: open new document
-      if let window = NSApplication.shared.mainWindow {
-        let response = await panel.beginSheetModal(for: window)
+      switch response {
+      case .OK:
+        guard let url = url else { return }
+        
+        let encoder = JSONEncoder()
+        let book = try! encoder.encode(Book())
 
-        switch response {
-        case .OK:
-          guard let url = panel.url else { return }
-          
-          let encoder = JSONEncoder()
-          let book = try! encoder.encode(Book())
-
-          guard fileManager.createFile(atPath: url.path, contents: book) else {
-            fatalError("Failed to create \(url.path)")
-          }
-        default:
-          break
+        guard fileManager.createFile(atPath: url.path, contents: book) else {
+          fatalError("Failed to create \(url.path)")
         }
+        
+        let controller = NSDocumentController.shared
+        let _ = try! await controller.openDocument(withContentsOf: url, display: true)
+      default:
+        break
       }
     }
   }
